3d38cc6e877d1ccd0ef0432cdd88d180781403ad
Bohdan Skochii <b.skochii@gmail.com>
2022-10-19 01:51:47 +0300

    finish part01 1.3-1.5 exercises

---
+++ part-01/course-info/src/App.js
@@ -0,0 +1,61 @@
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
+  };
+
+  const { parts } = course;
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  );
+};
+
+const Header = ({ course }) => {
+  return <h1>{course.name}</h1>;
+};
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      <Part part={parts[0]} />
+      <Part part={parts[1]} />
+      <Part part={parts[2]} />
+    </>
+  );
+};
+const Total = ({ parts }) => {
+  return (
+    <p>
+      Number of exercises:{' '}
+      {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+    </p>
+  );
+};
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name}: {part.exercises}
+    </p>
+  );
+};
+
+export default App;
